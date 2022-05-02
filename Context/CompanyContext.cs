3121f61225207c24ca03600d9ed607bc85f34655
sunergy08 <65669659+sunergy08@users.noreply.github.com>
2022-05-02 17:07:45 +0100

    Add project files.

---
+++ Context/CompanyContext.cs
@@ -0,0 +1,18 @@
+﻿namespace _5REACH1_API.Context
+{
+    using Microsoft.EntityFrameworkCore;
+    using _5REACH1_API.Models;
+    using _5REACH1_API.Models.WebApiCoreWithEF.Models;
+
+    public class CompanyContext
+        : DbContext
+    {
+        public CompanyContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
